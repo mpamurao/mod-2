1dbf0571d7d151324f9166825d701c8eb3e3d8ab
Marie Amurao <mamurao@g.clemson.edu>
2021-05-14 13:25:57 -0400

    saved dummy data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import config from '../config';
+import data from './dummydata';
 
-function Home(props) {
-    return (
-        <div>
-            Hello!
-        </div>
-    );
+class Home extends Component {
+    async componentDidMount() {
+        const apiKey = config.bls;
+        // base url
+        const url = `https://api.bls.gov/publicAPI/v2/timeseries/data/`;
+        const fetchURL = `${url}JLNS14000003`;
+
+        try{
+            // const response = await fetch(fetchURL);
+            // const data = await response.json();
+            console.log(data);
+
+        }
+        catch(err){
+            console.log(err);
+        }
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
 }
 
 export default Home;
---
+++ src/components/dummydata.js
@@ -0,0 +1,6 @@
+const data = {"status":"REQUEST_SUCCEEDED","responseTime":184,"message":["Series does not exist for Series JLNS14000003"],"Results":{
+    "series":
+    [{"seriesID":"JLNS14000003","data":[]}]
+    }}
+
+export default data;
