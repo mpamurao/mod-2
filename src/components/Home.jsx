e14a7bf3c5f78cc5c7d260856e378480634dd0ac
Marie Amurao <mamurao@g.clemson.edu>
2021-05-18 09:57:55 -0400

    pseudocode all the useful data titles available

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import config from '../config';
 import data from './dummydata';
 
+// API Walkthrough: https://hashrocket.com/blog/posts/extracting-programmer-employment-data-from-bls
+// BLS Wrapper: https://github.com/reaperkrew/bls
+
 class Home extends Component {
+    constructor() {
+        super()
+        
+        this.state = {
+            data: "",
+        }
+    }
     async componentDidMount() {
         const apiKey = config.bls;
-        // base url
+        // base url https://api.bls.gov/publicAPI/v2/timeseries/data/<seriesID>?registrationkey=${apiKey}
         const url = `https://api.bls.gov/publicAPI/v2/timeseries/data/`;
-        const fetchURL = `${url}JLNS14000003`;
+        // const fetchURL = `${url}OEUN000000000000015113001`;
 
         try{
-            // const response = await fetch(fetchURL);
-            // const data = await response.json();
+            const response = await fetch(fetchURL);
+            const data = await response.json();
             console.log(data);
+
+            this.setState({data});
 
         }
         catch(err){
@@ -23,7 +35,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                
+                {/* {this.state.data} */}
             </div>
         );
     }
