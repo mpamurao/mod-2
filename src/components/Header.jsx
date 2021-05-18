83fa3df6cddec179e858b5b4f227eeb2618d998a
Marie Amurao <mamurao@g.clemson.edu>
2021-05-18 17:58:45 -0400

    header css made

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Typography, AppBar, Toolbar} from '@material-ui/core';
-import {Link as RouterLink} from 'react-router-dom';
+import {Typography, AppBar, Toolbar, Button} from '@material-ui/core';
+import {Link} from 'react-router-dom';
 import headerStyles from './styles/headerStyles';
 
 function Header(props) {
@@ -11,18 +11,25 @@ function Header(props) {
         <div className="header">
             {/* create AppBar for heading */}
             <AppBar position="relative" className={classes.appBar}>
-                <Typography>
+                <Typography className={classes.heading} variant="h4" gutterBottom>
                     API SEARCH PROJECT USING DATA FROM U.S. BUREAU OF LABOR STATISTICS
                 </Typography>
-                <Typography>
-                    ***BLS.gov cannot vouch for the data or analyses derived from these data after 
-                    the data have been retrieved from BLS.gov.
+                <Typography variant="subtitle2" gutterBottom>
+                    ***BLS.gov cannot vouch for the data or analyses derived from these 
+                    data after the data have been retrieved from BLS.gov.***
                 </Typography>
 
-                <Toolbar>
-                    <Typography variant="subtitle1"><RouterLink to="/home"> Home </RouterLink></Typography>
-                    <Typography variant="subtitle1"><RouterLink to="/about"> About </RouterLink></Typography>
-
+                {/* make navBar with buttons */}
+                <Toolbar className={classes.toolbar}>
+                    <Button>
+                        <Link to="/home" className={classes.toolButton}> Home </Link>
+                    </Button>
+                    <Button>
+                        <Link to="/about" className={classes.toolButton}> About </Link>
+                    </Button>
+                    <Button>
+                        <Link to="/about" className={classes.toolButton}> Menu </Link>
+                    </Button>
                 </Toolbar>
             </AppBar>
         
--- src/components/styles/headerStyles.js
+++ src/components/styles/headerStyles.js
@@ -1,14 +1,28 @@
 import {makeStyles} from "@material-ui/core/styles";
 
-// hook called headerStyles that is a function that has a callback function that returns an object with CSS
+// hook called headerStyles that is a function that has 
+// a callback function that returns an object with CSS
 
-const headerStyles = makeStyles(theme => ({
-    appBar: {
-        backgroundColor:"blue",
-        padding:"1rem",
+const headerStyles = makeStyles(() => ({
+    appBar:{
+        backgroundColor:"white",
         textAlign:"center",
-        color:'white',
-    }
+        color:'blue',
+    },
+    heading:{
+        fontWeight:"900",
+        textShadow:"1px 1px 1px",
+        paddingTop:"1.5rem",
+    },
+    toolbar:{
+        backgroundColor:"blue",
+        textAlign:"center",
+    },
+    toolButton:{
+        color:"white",
+        textDecoration:"none",
+        fontSize:"1rem",
+    },
 }))
 
 export default headerStyles;
