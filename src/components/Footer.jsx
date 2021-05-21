52935ea257e33d193a2133eb0471291f8ee5ae85
Marie Amurao <mamurao@g.clemson.edu>
2021-05-21 01:34:00 -0400

    typos footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,8 +11,8 @@ function Footer() {
             <BottomNavigation className={classes.footer}>
                 <BottomNavigationAction classes={{label:classes.link}}
                 component={Link} href="https://www.bls.gov/bls/overview.htm" 
-                target="_blank" value="U..S. Bureau of Labor Statistics" 
-                showLabel label="U..S. Bureau of Labor Statistics" />
+                target="_blank" value="U.S. Bureau of Labor Statistics" 
+                showLabel label="U.S. Bureau of Labor Statistics" />
             </BottomNavigation>
         </div>
     );
